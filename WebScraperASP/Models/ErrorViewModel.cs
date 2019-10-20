78c74710341b48c9c5e2df98d401a02672ff4659
vjpal3 <vrishalipal@yahoo.com>
2019-10-19 20:36:08 -0400

    Add project files.

---
+++ WebScraperASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebScraperASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
